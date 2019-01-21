d2566ccbdbb131e5f8d98d2f01f1501fe0724270
Espen Skaufel <espen@skaufel.net>
2019-01-21 20:09:14 +0100

    Added enumerator on variablescollection

---
+++ src/Yale/Core/VariableEnumerator.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Yale.Core.Interface;
+
+namespace Yale.Core
+{
+    internal sealed class VariableEnumerator : IEnumerator<KeyValuePair<string, object>>
+    {
+        private IEnumerator<KeyValuePair<string, IVariable>> _enumerator;
+
+        public VariableEnumerator(IDictionary<string, IVariable> values)
+        {
+            _enumerator = values.GetEnumerator();
+        }
+
+        public KeyValuePair<string, object> Current => new KeyValuePair<string, object>(_enumerator.Current.Key, _enumerator.Current.Value.ValueAsObject);
+
+        object IEnumerator.Current => Current;
+
+        public bool MoveNext()
+        {
+            return _enumerator.MoveNext();
+        }
+
+        public void Reset()
+        {
+            _enumerator.Reset();
+        }
+
+        public void Dispose()
+        {
+        }
+    }
+}
--- test/Yale.Tests/Core/ValueCollection.cs
+++ test/Yale.Tests/Core/ValueCollection.cs
@@ -1,15 +1,15 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
-using Yale.Engine;
 using Yale.Core;
+using Yale.Engine;
+
 namespace Yale.Tests.Core
 {
     [TestClass]
     public class ValueCollection
     {
         private readonly ComputeInstance _instance = new ComputeInstance();
-        private VariableCollection _variable => _instance.Variables;
-
+        private VariableCollection _variables => _instance.Variables;
 
         [TestMethod]
         public void AddValue_CanBe_Retrieved()
@@ -34,12 +34,12 @@ namespace Yale.Tests.Core
         {
             Assert.ThrowsException<ArgumentNullException>(() =>
             {
-                _variable.Get(null);
+                _variables.Get(null);
             });
 
             Assert.ThrowsException<ArgumentNullException>(() =>
             {
-                _variable.Get<object>(null);
+                _variables.Get<object>(null);
             });
         }
 
@@ -48,24 +48,40 @@ namespace Yale.Tests.Core
         {
             const int a = 1;
             _instance.Variables.Add("a", a);
-            var aResult = _variable.Get<int>("a");
+            var aResult = _variables.Get<int>("a");
             Assert.AreEqual(a, aResult);
 
             const double b = 1.0;
             _instance.Variables.Add("b", b);
-            var bResult = _variable.Get<double>("b");
+            var bResult = _variables.Get<double>("b");
             Assert.AreEqual(b, bResult);
 
             const string c = "stringValue";
             _instance.Variables.Add("c", c);
-            var cResult = _variable.Get<string>("c");
+            var cResult = _variables.Get<string>("c");
             Assert.AreEqual(c, cResult);
 
             const string d = "a > b";
             _instance.Variables.Add("d", d);
-            var dResult = _variable.Get<string>("d");
+            var dResult = _variables.Get<string>("d");
             Assert.AreEqual(d, dResult);
             Assert.AreNotEqual(false, dResult);
+        }
+
+        [TestMethod]
+        public void Enumerator_Works()
+        {
+            foreach (var variable in _variables)
+            {
+                Assert.Fail("No variables addded");
+            }
+
+            _variables.Add("a", 1);
+            foreach (var variable in _variables)
+            {
+                Assert.AreEqual("a", variable.Key);
+                Assert.AreEqual(1, variable.Value);
+            }
         }
     }
 }
